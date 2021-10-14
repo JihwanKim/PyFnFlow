a732fdb60d4cbd7c65f1871f000d847582b77095
jihwanKim <jihwan.dev@gmail.com>
2021-10-14 22:51:38 +0900

    implement setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(name='pyfnflow',
+      version='0.1.0',
+      description='python function compose library',
+      url='https://github.com/jihwanKim/PyFnFlow',
+      author='JihwanKim',
+      author_email='jihwan.dev@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      classifiers=[
+          'Programming Language :: Python :: 3.9',
+      ],
+      zip_safe=False)
